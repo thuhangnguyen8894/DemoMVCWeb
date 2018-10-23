b59f00b551f2cfd460c205fc4340665cf6fd1061
thuhangnguyen8894 <thuhang.nguyen8894@gmail.com>
2018-10-23 10:29:07 +0700

    Create Web MVC

---
+++ DemoMVCWeb/modelRelationshipDemo/modelRelationshipDemo/Models/myDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace modelRelationshipDemo.Models
+{
+    public class myDbContext
+    {
+    }
+    [Table("Supplier")]
+    public class Supplier
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int SupplierId { get; set; }
+
+        [Display(Name ="Company Name")]
+        public string CompanyName { get; set; }
+
+        [Display(Name ="Supplier City")]
+        public string City { get; set; }
+
+        [Display(Name ="Supplier Country")]
+        public string Country { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+
+    [Table("Product")]
+    public class Product
+    {
+
+    }
+}
---
+++ DemoMVCWeb/modelRelationshipDemo/modelRelationshipDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(modelRelationshipDemo.Startup))]
+namespace modelRelationshipDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
