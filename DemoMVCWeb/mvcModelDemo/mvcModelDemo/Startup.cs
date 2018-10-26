c403a542dfe12a568efdcd9d4fa1e27f9ce5b8ac
thuhangnguyen8894 <thuhang.nguyen8894@gmail.com>
2018-10-26 17:11:33 +0700

    mvcModelDemo

---
+++ DemoMVCWeb/mvcModelDemo/mvcModelDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvcModelDemo.Startup))]
+namespace mvcModelDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
