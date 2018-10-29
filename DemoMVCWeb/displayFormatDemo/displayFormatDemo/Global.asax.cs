26fb8362536189d978954c2eac45a3cc3ce62c3f
thuhangnguyen8894 <thuhang.nguyen8894@gmail.com>
2018-10-29 17:32:49 +0700

    Update Using the DisplayFormat Data Annotation

---
+++ DemoMVCWeb/displayFormatDemo/displayFormatDemo/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using displayFormatDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace displayFormatDemo
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //Seed database with sample data by calling initializer
+            Database.SetInitializer<ItemListDB>(new ItemListInitializer());
+
+        }
+    }
+}
---
+++ DemoMVCWeb/displayFormatDemo/displayFormatDemo/Models/ItemListInitializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace displayFormatDemo.Models
+{
+    public class ItemListInitializer : DropCreateDatabaseAlways<ItemListDB>
+    {
+        protected override void Seed(ItemListDB context)
+        {
+            base.Seed(context);
+            var itemLists = new List<ItemList>
+            {
+                new ItemList
+                {
+                    Title = "Groceries",
+                    ListItemEntry = "Pick up lettuce, tomatoes, carrots and ...",
+                    EntryCreateDate = new DateTime(2014,7,4,11,0,0)
+                },
+
+                new ItemList
+                {
+                    Title = "Shopping Mall",
+                    ListItemEntry = "Purchase jeans, cargo pants, dress shirts",
+                    EntryCreateDate = new DateTime(2014,7,7,9,9,0)
+                },
+            };
+            itemLists.ForEach(y => context.ItemLists.Add(y));
+            context.SaveChanges();
+        }
+    }
+}
