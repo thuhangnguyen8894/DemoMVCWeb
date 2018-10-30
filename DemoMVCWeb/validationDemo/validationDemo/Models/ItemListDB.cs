ac067848549070a78360f52c29a34b5849c70490
thuhangnguyen8894 <thuhang.nguyen8894@gmail.com>
2018-10-30 17:29:27 +0700

    Update Validation Demo

---
+++ DemoMVCWeb/validationDemo/validationDemo/Models/ItemListDB.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace validationDemo.Models
+{
+    public class ItemListDB : DbContext
+    {
+        public DbSet<ItemList> ItemLists { get; set; }
+    }
+
+    [Table("ItemList")]
+    public class ItemList
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int ListItemID { get; set; }
+
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Things To Do")]
+        public string ListItemEntry { get; set; }
+
+        [Display(Name = "Date Created")]
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime EntryCreateDate { get; set; }
+    }
+}
